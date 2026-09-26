f782b23c02516df035598e1014be0b534aa509a7
vahinikumar <malipeddivahinisaikumar@gmail.com>
2026-09-26 21:17:38 +0530

    Open sound settings instead of adjusting volume
    Replaced audio volume adjustment with intent to open sound settings.

--- app/src/main/java/com/example/wifitoggletest/MainActivity.java
+++ app/src/main/java/com/example/wifitoggletest/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.wifitoggletest;
 
 import android.app.Activity;
-import android.media.AudioManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends Activity {
 
@@ -10,15 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AudioManager audioManager =
-                (AudioManager) getSystemService(AUDIO_SERVICE);
+        Intent intent =
+                new Intent(Settings.ACTION_SOUND_SETTINGS);
 
-        audioManager.setStreamVolume(
-                AudioManager.STREAM_RING,
-                audioManager.getStreamMaxVolume(
-                        AudioManager.STREAM_RING),
-                AudioManager.FLAG_SHOW_UI
-        );
+        startActivity(intent);
 
         finish();
     }
