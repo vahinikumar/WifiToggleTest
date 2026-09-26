f022ca54475cba186bed00369a0fca30ca762d32
vahinikumar <malipeddivahinisaikumar@gmail.com>
2026-09-26 17:37:52 +0530

    Implement ringer mode toggle in MainActivity
    Toggle the device's ringer mode between normal and vibrate in MainActivity.

--- app/src/main/java/com/example/wifitoggletest/MainActivity.java
+++ app/src/main/java/com/example/wifitoggletest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.wifitoggletest;
 
 import android.app.Activity;
+import android.media.AudioManager;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,7 +10,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WifiAccessibilityService.openNotificationPanel();
+        AudioManager audioManager =
+                (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        if (audioManager != null) {
+
+            if (audioManager.getRingerMode()
+                    == AudioManager.RINGER_MODE_NORMAL) {
+
+                audioManager.setRingerMode(
+                        AudioManager.RINGER_MODE_VIBRATE
+                );
+
+            } else {
+
+                audioManager.setRingerMode(
+                        AudioManager.RINGER_MODE_NORMAL
+                );
+            }
+        }
 
         finish();
     }
