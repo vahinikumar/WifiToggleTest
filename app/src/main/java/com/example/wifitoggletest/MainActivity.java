5bc3d92ebfb37553569cf2935e339da8ccd2dfaf
vahinikumar <malipeddivahinisaikumar@gmail.com>
2026-09-26 21:43:20 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/wifitoggletest/MainActivity.java
+++ app/src/main/java/com/example/wifitoggletest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.wifitoggletest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class MainActivity extends Activity {
 
@@ -9,7 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WifiAccessibilityService.openNotificationPanel();
+        Intent intent =
+                new Intent(Settings.ACTION_SOUND_SETTINGS);
+
+        startActivity(intent);
 
         finish();
     }
